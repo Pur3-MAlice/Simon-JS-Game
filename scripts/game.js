980da458fabece0a9201493c307115155abb959d
Pur3-MAlice <aliceford98@live.co.uk>
2023-06-21 15:50:54 +0000

    adds new test for newGame and runs through fails until basics all pass

--- scripts/game.js
+++ scripts/game.js
@@ -5,5 +5,13 @@ let game = {
     choices: ["button1", "button2", "button3", "button4"],
 };
 
+function newGame() {
+    /**
+     * Starts game on click of center of bullseye and on the new game 
+     * Also maybe starts on after game failure
+     */
+    game.score = 0;
+}
 
-module.exports = { game };
+module.exports = { game, newGame};
+
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { game } = require("../game");
+const { game, newGame } = require("../game");
 
 
 beforeAll(() => {
@@ -28,5 +28,16 @@ describe("game object contains correct keys", () => {
     });
     test("choices contains corret ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
+});
+
+
+describe("newGame works correctly", () => {
+    beforeAll(() => {
+        game.score = 42;
+        newGame();
+    });
+    test("should set game score to 0", () => {
+        expect(game.score).toEqual(0);
     });
 });

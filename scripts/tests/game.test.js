76e5edad78feeb97ab68a11bd0a51ba280f135ac
Pur3-MAlice <aliceford98@live.co.uk>
2023-06-26 21:37:37 +0000

    adds last test to game object to make sure it contains all the right variables

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -33,6 +33,12 @@ describe("game object contains correct keys", () => {
     test("turnNumber key exists", () => {
         expect("turnNumber" in game).toBe(true);
     });
+    test("turnInProgress key exists", () => {
+        expect("turnInProgress" in game).toBe(true);
+    });
+    test("lastButton key exists", () => {
+        expect("lastButton" in game).toBe(true);
+    });
 });
 
 
@@ -104,5 +110,11 @@ describe("gameplay works correctly", () => {
     test("should toggle turnInProgress to true", () => {
         showTurns();
         expect(game.turnInProgress).toBe(true);
+    });
+    test("clicking during computers turn should fail", () => {
+        showTurns();
+        game.lastButton = "";
+        document.getElementById("button2").click();
+        expect(game.lastButton).toBe("");
     });
 });
